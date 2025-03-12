9d6fa422fdf3a22d385d2edf92491a4d05114a3b
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-03-12 03:19:41 +0200

    Add actor model and migration for actor table

--- database/migrations/2025_03_12_011116_create_actors_table.php
+++ database/migrations/2025_03_12_011116_create_actors_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Actor;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('movies', function (Blueprint $table) {
+        Schema::create('actors', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(actor::class);
-            $table->string('title');
-            $table->text('description');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('movies');
+        Schema::dropIfExists('actors');
     }
 };

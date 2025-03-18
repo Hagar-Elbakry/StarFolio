101afe00ea6dd55ca121fc6d1ecaab5c31283ab7
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-03-18 04:05:31 +0200

    Implement MovieController and refactor movie routes to use controller method

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Actor;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index() {
+        $movies = Actor::find(1)->movies()->latest()->simplePaginate(3);
+        return view('movies.index', ['movies' => $movies]);
+    }
+
+    public function create() {
+        return view('movies.create');
+    }
+    public function show(Movie $movie) {
+        $movie->with('actors');
+        return view('movies.show', ['movie' => $movie]);
+    }
+
+    public function store() {
+        request()->validate([
+            'title' => 'required|unique:movies,title',
+            'image' => 'required',
+            'description' => 'required',
+        ]);
+
+        $movie = Movie::create([
+            'title' => request('title'),
+            'image' => request('image'),
+            'description' => request('description'),
+        ]);
+        $movie->actors()->attach(1);
+        return redirect('/movies');
+    }
+
+    public function edit(Movie $movie) {
+        return view('movies.edit', ['movie' => $movie]);
+    }
+
+    public function update(Movie $movie) {
+        request()->validate([
+            'title' => 'required|unique:movies,title,' . $movie->id,
+            'image' => 'required',
+            'description' => 'required',
+        ]);
+
+        $movie->update([
+            'title' => request('title'),
+            'image' => request('image'),
+            'description' => request('description')
+        ]);
+
+        return redirect("/movies/$movie->id");
+    }
+
+    public function destroy(Movie  $movie) {
+        $movie->delete();
+        return redirect("/movies");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use App\Models\Actor;
 use Illuminate\Support\Facades\Route;
 use App\Models\Movie;
@@ -7,60 +8,12 @@ use App\Models\Movie;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::get('/movies', function() {
-   $movies = Actor::find(1)->movies()->latest()->simplePaginate(3);
-    return view('movies.index', ['movies' => $movies]);
-});
-
-Route::get('/movies/create', function() {
-    return view('movies.create');
-});
-
-Route::get('/movies/{movie}', function(Movie $movie) {
-    $movie->with('actors');
-
-    return view('movies.show', ['movie' => $movie]);
-});
-
-Route::post('/movies', function() {
-    request()->validate([
-        'title' => 'required|unique:movies,title',
-        'image' => 'required',
-        'description' => 'required',
-    ]);
-
-   $movie = Movie::create([
-        'title' => request('title'),
-        'image' => request('image'),
-        'description' => request('description'),
-    ]);
-    $movie->actors()->attach(1);
-    return redirect('/movies');
-});
-
-Route::get('/movies/{movie}/edit', function(Movie $movie) {
-    return view('movies.edit', ['movie' => $movie]);
-});
-
-Route::patch('/movies/{movie}', function(Movie $movie) {
-    request()->validate([
-        'title' => 'required|unique:movies,title,' . $movie->id,
-        'image' => 'required',
-        'description' => 'required',
-    ]);
-
-    $movie->update([
-        'title' => request('title'),
-        'image' => request('image'),
-        'description' => request('description')
-    ]);
-
-    return redirect("/movies/$movie->id");
-});
-
-Route::delete('/movies/{movie}', function(Movie $movie) {
-    $movie->delete();
-    return redirect("/movies");
-});
+Route::get('/movies',[MovieController::class,'index']);
+Route::get('/movies/create',[MovieController::class,'create']);
+Route::get('/movies/{movie}', [MovieController::class, 'show']);
+Route::post('/movies',[MovieController::class,'store']);
+Route::get('/movies/{movie}/edit',[MovieController::class, 'edit']);
+Route::patch('/movies/{movie}',[MovieController::class, 'update']);
+Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
 
 

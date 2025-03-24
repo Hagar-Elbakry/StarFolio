3b10bd0fd680276ef3bf55302ceeb02b9b43add0
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-03-24 02:29:14 +0200

    update layout to include navigation links for movies and profile and adjust user authentication diaplay

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -62,25 +62,31 @@
             <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-                <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                <a href="/movies" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Movies</a>
+                <a href="/profile" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Profile</a>
             </div>
-            <div class="border-t border-gray-700 pt-4 pb-3">
-                <div class="flex items-center px-5">
-                    <div class="shrink-0">
-                        <img class="size-10 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Theo_James_Divergent_premiere.jpg/330px-Theo_James_Divergent_premiere.jpg" alt="">
-                    </div>
+            <div class="border-t border-gray-700 pt-4 pb-3 ">
+                <div class="flex items-center px-5 flex justify-between">
                     <div class="ml-3">
-                        <div class="text-base/5 font-medium text-white">Theo James</div>
-                        <div class="text-sm font-medium text-gray-400">theojames@gmail.com</div>
+                        <div class="shrink-0">
+                            <a href="/">
+                                <img class="size-8" src="https://img.icons8.com/?size=100&id=VNOu35D2HWOH&format=png&color=000000" alt="Your Company">
+                            </a>
+                        </div>
                     </div>
-                    <button type="button" class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                        <span class="absolute -inset-1.5"></span>
-                        <span class="sr-only">View notifications</span>
-                        <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                        </svg>
-                    </button>
+                    <div>
+                    @guest
+                        <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+                        <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+                    @endguest
+
+                    @auth
+                        <form method="post" action="/logout">
+                            @csrf
+                            <x-form-button>Log Out</x-form-button>
+                        </form>
+                    @endauth
+                    </div>
                 </div>
             </div>
         </div>
